d0e386bb8b855319e1e388b299832509c3f5d8af
soeffing <ulrich_soeffing@gmx.de>
2013-06-28 10:51:56 +0100

    very basic auth done

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,4 +6,23 @@ angular.module('myApp.controllers', [])
   }])
   .controller('MyCtrl2', [function() {
 
+  }])
+  .controller('LoginCtrl', ['$scope', '$http', function($scope, $http) {
+  	$scope.user = {};
+  	$scope.user.email = "";
+  	$scope.user.password = "";
+  	$scope.login = function() {
+  	  console.log($scope.email);
+  	  var request = $http.post('http://localhost:3000/api/v1/users/sign_in.json', {user: {email: $scope.user.email, password: $scope.user.password }}).
+  	  success(function(data, status, headers, config) {
+  	     // this callback will be called asynchronously
+  	     // when the response is available
+  	       console.log(data);
+  	     }).
+  	     error(function(data, status, headers, config) {
+  	     // called asynchronously if an error occurs
+  	     // or server returns response with an error status.
+  	     console.log(data,status, headers, config);
+  	  });
+  	}
   }]);
